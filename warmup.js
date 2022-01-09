00df26e5bf38ecb8a0c01b03b008024c7c959bff
Marin Maxim <superelmarin@yandex.ru>
2022-01-09 15:31:21 +0500

    2 tasks from js1

--- warmup.js
+++ warmup.js
@@ -1,4 +1,8 @@
+
 'use strict';
+
+// eslint-disable-next-line no-console
+console.log(centuryByYearProblem(2021.9));
 
 /**
  * Складывает два целых числа
@@ -9,6 +13,12 @@
  */
 function abProblem(a, b) {
     // Ваше решение
+    if (typeof(a) !== 'number' || typeof(b) !== 'number') {
+        throw new TypeError('В аргументы переданы не числа');
+    }
+
+    return a + b;
+
 }
 
 /**
@@ -20,89 +30,94 @@ function abProblem(a, b) {
  */
 function centuryByYearProblem(year) {
     // Ваше решение
+
+    if (typeof(year) !== 'number') {
+        throw new TypeError('В качестве года передано не число');
+    }
+
+    if (typeof(year) === 'number' && year < 0) {
+        throw new RangeError('Отрицательное число');
+    }
+
+    return Math.floor(year / 100) + 1;
+
 }
 
-/**
- * Переводит цвет из формата HEX в формат RGB
- * @param {String} hexColor Цвет в формате HEX, например, '#FFFFFF'
- * @throws {TypeError} Когда цвет передан не строкой
- * @throws {RangeError} Когда значения цвета выходят за пределы допустимых
- * @returns {String} Цвет в формате RGB, например, '(255, 255, 255)'
- */
-function colorsProblem(hexColor) {
-    // Ваше решение
-}
 
-/**
- * Находит n-ое число Фибоначчи
- * @param {Number} n Положение числа в ряде Фибоначчи
- * @throws {TypeError} Когда в качестве положения в ряде передано не число
- * @throws {RangeError} Когда положение в ряде не является целым положительным числом
- * @returns {Number} Число Фибоначчи, находящееся на n-ой позиции
- */
-function fibonacciProblem(n) {
-    // Ваше решение
-}
+// /**
+//  * Переводит цвет из формата HEX в формат RGB
+//  * @param {String} hexColor Цвет в формате HEX, например, '#FFFFFF'
+//  * @throws {TypeError} Когда цвет передан не строкой
+//  * @throws {RangeError} Когда значения цвета выходят за пределы допустимых
+//  * @returns {String} Цвет в формате RGB, например, '(255, 255, 255)'
+//  */
+// function colorsProblem(hexColor) {
+//     // Ваше решение
+// }
 
-/**
- * Транспонирует матрицу
- * @param {(Any[])[]} matrix Матрица размерности MxN
- * @throws {TypeError} Когда в функцию передаётся не двумерный массив
- * @returns {(Any[])[]} Транспонированная матрица размера NxM
- */
-function matrixProblem(matrix) {
-    // Ваше решение
-}
+// /**
+//  * Находит n-ое число Фибоначчи
+//  * @param {Number} n Положение числа в ряде Фибоначчи
+//  * @throws {TypeError} Когда в качестве положения в ряде передано не число
+//  * @throws {RangeError} Когда положение в ряде не является целым положительным числом
+//  * @returns {Number} Число Фибоначчи, находящееся на n-ой позиции
+//  */
+// function fibonacciProblem(n) {
+//     // Ваше решение
+// }
 
-/**
- * Переводит число в другую систему счисления
- * @param {Number} n Число для перевода в другую систему счисления
- * @param {Number} targetNs Система счисления, в которую нужно перевести (Число от 2 до 36)
- * @throws {TypeError} Когда переданы аргументы некорректного типа
- * @throws {RangeError} Когда система счисления выходит за пределы значений [2, 36]
- * @returns {String} Число n в системе счисления targetNs
- */
-function numberSystemProblem(n, targetNs) {
-    // Ваше решение
-}
+// /**
+//  * Транспонирует матрицу
+//  * @param {(Any[])[]} matrix Матрица размерности MxN
+//  * @throws {TypeError} Когда в функцию передаётся не двумерный массив
+//  * @returns {(Any[])[]} Транспонированная матрица размера NxM
+//  */
+// function matrixProblem(matrix) {
+//     // Ваше решение
+// }
 
-/**
- * Проверяет соответствие телефонного номера формату
- * @param {String} phoneNumber Номер телефона в формате '8–800–xxx–xx–xx'
- * @returns {Boolean} Если соответствует формату, то true, а иначе false
- */
-function phoneProblem(phoneNumber) {
-    // Ваше решение
-}
+// /**
+//  * Переводит число в другую систему счисления
+//  * @param {Number} n Число для перевода в другую систему счисления
+//  * @param {Number} targetNs Система счисления, в которую нужно перевести (Число от 2 до 36)
+//  * @throws {TypeError} Когда переданы аргументы некорректного типа
+//  * @throws {RangeError} Когда система счисления выходит за пределы значений [2, 36]
+//  * @returns {String} Число n в системе счисления targetNs
+//  */
+// function numberSystemProblem(n, targetNs) {
+//     // Ваше решение
+// }
 
-/**
- * Определяет количество улыбающихся смайликов в строке
- * @param {String} text Строка в которой производится поиск
- * @throws {TypeError} Когда в качестве аргумента передаётся не строка
- * @returns {Number} Количество улыбающихся смайликов в строке
- */
-function smilesProblem(text) {
-    // Ваше решение
-}
+// /**
+//  * Проверяет соответствие телефонного номера формату
+//  * @param {String} phoneNumber Номер телефона в формате '8–800–xxx–xx–xx'
+//  * @returns {Boolean} Если соответствует формату, то true, а иначе false
+//  */
+// function phoneProblem(phoneNumber) {
+//     // Ваше решение
+// }
 
-/**
- * Определяет победителя в игре "Крестики-нолики"
- * Тестами гарантируются корректные аргументы.
- * @param {(('x' | 'o')[])[]} field Игровое поле 3x3 завершённой игры
- * @returns {'x' | 'o' | 'draw'} Результат игры
- */
-function ticTacToeProblem(field) {
-    // Ваше решение
-}
+// /**
+//  * Определяет количество улыбающихся смайликов в строке
+//  * @param {String} text Строка в которой производится поиск
+//  * @throws {TypeError} Когда в качестве аргумента передаётся не строка
+//  * @returns {Number} Количество улыбающихся смайликов в строке
+//  */
+// function smilesProblem(text) {
+//     // Ваше решение
+// }
+
+// /**
+//  * Определяет победителя в игре "Крестики-нолики"
+//  * Тестами гарантируются корректные аргументы.
+//  * @param {(('x' | 'o')[])[]} field Игровое поле 3x3 завершённой игры
+//  * @returns {'x' | 'o' | 'draw'} Результат игры
+//  */
+// function ticTacToeProblem(field) {
+//     // Ваше решение
+// }
 
 module.exports = {
     abProblem,
-    centuryByYearProblem,
-    colorsProblem,
-    fibonacciProblem,
-    matrixProblem,
-    numberSystemProblem,
-    phoneProblem,
-    smilesProblem,
-    ticTacToeProblem
+    centuryByYearProblem
 };
